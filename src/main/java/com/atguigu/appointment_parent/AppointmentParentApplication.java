6c54fa5ad7ecf271af9dbadc40e7ee43ab4b60b1
sunjunjie <sj970920@126.com>
2022-03-28 12:02:18 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/appointment_parent/AppointmentParentApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.appointment_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/appointment_parent/AppointmentParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.appointment_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
